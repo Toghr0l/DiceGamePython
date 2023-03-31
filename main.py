5cf3051255c19541562c583bcf558d558a35a165
Toghrol <127787956+Toghr0l@users.noreply.github.com>
2023-03-31 15:22:08 +0330

    Just committed now!

---
+++ main.py
@@ -0,0 +1,55 @@
+import random
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+dice = []
+total = 0
+num_of_dice = int(input('How many dice you want to roll? '))
+
+for die in range(num_of_dice):
+    dice.append(random.randint(1, 6))
+
+# for die in range(num_of_dice):
+#     for line in dice_art.get(dice[die]):
+#         print(line)
+
+for line in range(5):
+    for die in dice:
+        print(dice_art.get(die)[line], end='')
+    print()
+
+for die in dice:
+    total += die
+
+print(f'Total: {total}')
